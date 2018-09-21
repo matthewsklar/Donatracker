151f196fc021e118b1a40cc597622abe282dec72
Matthew Sklar <matthewsklar227@gmail.com>
2018-09-21 16:15:03 -0400

    Create view for welcome page and handle login button presses

--- app/src/main/java/com/donatracker/a3even2odd/donatracker/controllers/WelcomeActivity.java
+++ app/src/main/java/com/donatracker/a3even2odd/donatracker/controllers/WelcomeActivity.java
@@ -1,7 +1,11 @@
 package com.donatracker.a3even2odd.donatracker.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.donatracker.a3even2odd.donatracker.R;
 
@@ -11,5 +15,14 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    /**
+     * Button handler for login button.
+     *
+     * @param v the button
+     */
+    public void onLoginPressed(View v) {
+        Log.d("Login", "Go to login page");
     }
 }
