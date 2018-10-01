423732511fe27e946fbfe04642c6b06e9a96993d
Matthew Sklar <matthewsklar227@gmail.com>
2018-10-01 16:25:40 -0400

    Merge login with development

--- app/src/main/java/com/donatracker/a3even2odd/donatracker/models/user/UserTypes.java
+++ app/src/main/java/com/donatracker/a3even2odd/donatracker/models/user/UserTypes.java
@@ -1,20 +1,17 @@
-package com.donatracker.a3even2odd.donatracker.models;
+package com.donatracker.a3even2odd.donatracker.models.user;
 
 public enum UserTypes {
-
     ADMIN               ("Admin"),
     USER                ("User"),
     LOCATION_EMPLOYEE   ("Location Employee");
 
     private final String userType;
 
-
     UserTypes(String userType) {
         this.userType = userType;
     }
 
-    public String getUserTpye() {
+    public String getUserType() {
         return userType;
     }
-
 }
