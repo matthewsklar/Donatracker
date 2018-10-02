66074059e0a40ea7a6d69efc2f35e7cdb775f12b
Matthew Sklar <matthewsklar227@gmail.com>
2018-10-02 00:09:29 -0400

    Created abstract parser

---
+++ app/src/main/java/com/donatracker/a3even2odd/donatracker/models/parser/Parser.java
@@ -0,0 +1,39 @@
+package com.donatracker.a3even2odd.donatracker.models.parser;
+
+/**
+ * Abstract class for parsers.
+ *
+ * @author Matthew Sklar
+ * @version 1.0
+ * @since 1.0
+ */
+public abstract class Parser {
+    /**
+     * Path to the location of the file to parse.
+     */
+    private String loc;
+
+    /**
+     * Constructor for Parser
+     *
+     * @param loc path to the location of the file to parse
+     */
+    public Parser(String loc) {
+        this.loc = loc;
+    }
+
+    /**
+     * Get the file and store its contents as a String.
+     *
+     * @return the contents of the file at loc
+     */
+    public String getFile() {
+        // TODO: Implement this class
+        return "TODO: IMPLEMENT THIS";
+    }
+
+    /**
+     * Parse the file into a data class.
+     */
+    public abstract void Parse();
+}
