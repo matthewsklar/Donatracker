6b8e8fffd0d8a65865d4d4cc3c3a36e775000dc4
Matthew Sklar <matthewsklar227@gmail.com>
2018-10-05 19:04:35 -0400

    Create raw folder for custom resources

--- app/src/main/java/com/donatracker/a3even2odd/donatracker/controllers/WelcomeActivity.java
+++ app/src/main/java/com/donatracker/a3even2odd/donatracker/controllers/WelcomeActivity.java
@@ -5,12 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import com.donatracker.a3even2odd.donatracker.R;
 
 public class WelcomeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +17,8 @@ public class WelcomeActivity extends AppCompatActivity {
 
     /**
      * Button handler for login button.
+     *
+     * When the login button is pressed, go to the login page.
      *
      * @param v the button
      */
@@ -30,6 +30,13 @@ public class WelcomeActivity extends AppCompatActivity {
         startActivity(loginIntent);
     }
 
+    /**
+     * Button handler for registration button.
+     *
+     * When the registration button is pressed, go to the registration page.
+     *
+     * @param v the button
+     */
     public void onRegisterPressed(View v) {
         Log.d("Registration","Go to Registation page");
 
