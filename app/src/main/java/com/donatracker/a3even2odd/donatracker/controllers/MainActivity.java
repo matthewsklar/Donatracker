7105fc30c5d297564044f7ca93ff979e21e4fcb4
Matthew Sklar <matthewsklar227@gmail.com>
2018-10-23 22:35:27 -0400

    Show donation buttons for location employees

--- app/src/main/java/com/donatracker/a3even2odd/donatracker/controllers/MainActivity.java
+++ app/src/main/java/com/donatracker/a3even2odd/donatracker/controllers/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 import com.donatracker.a3even2odd.donatracker.R;
 import com.donatracker.a3even2odd.donatracker.models.parser.CsvParser;
+import com.donatracker.a3even2odd.donatracker.models.user.UserTypes;
 
 import java.io.InputStream;
 import java.util.List;
@@ -18,9 +19,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loadElements();
         loadLocation();
     }
 
+    /**
+     * Load elements based on what type of user is logged in.
+     */
+    private void loadElements() {
+        UserTypes userType = (UserTypes) getIntent().getSerializableExtra("EXTRA_USER_TYPE");
+
+        switch (userType) {
+            case ADMIN:
+                break;
+            case USER:
+                break;
+            case LOCATION_EMPLOYEE:
+                findViewById(R.id.buttonAddDonation).setVisibility(View.VISIBLE);
+                findViewById(R.id.buttonViewDonations).setVisibility(View.VISIBLE);
+
+                break;
+            default:
+                break;
+                //throw new IllegalArgumentException("User must be a usertype");
+        }
+    }
+
+    /**
+     * Load the locations from the data.
+     */
     private void loadLocation() {
         InputStream inputStream = getResources().openRawResource(R.raw.location_data);
         CsvParser csvParser = new CsvParser(inputStream);
