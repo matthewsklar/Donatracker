4446faa15f00cec04a34ccc55b509ef6145b18db
Matthew Sklar <matthewsklar227@gmail.com>
2018-10-24 12:55:17 -0400

    Add submit button

--- app/src/main/java/com/donatracker/a3even2odd/donatracker/controllers/AddDonationActivity.java
+++ app/src/main/java/com/donatracker/a3even2odd/donatracker/controllers/AddDonationActivity.java
@@ -2,15 +2,23 @@ package com.donatracker.a3even2odd.donatracker.controllers;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 import com.donatracker.a3even2odd.donatracker.R;
 
 public class AddDonationActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_donation);
     }
 
+    /**
+     * Handler for add button.
+     *
+     * @param v the button
+     */
+    public void onAddPressed(View v) {
+
+    }
 }
