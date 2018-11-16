c23a1d3b1f0f7ad2cbdb918350b5c842f45ae8a5
Tarik Gasser <tgasser@gatech.edu>
2018-11-16 14:28:35 -0500

    Junit test on Load method

---
+++ app/src/test/java/com/donatracker/a3even2odd/donatracker/models/donation/DonationTest.java
@@ -0,0 +1,41 @@
+package com.donatracker.a3even2odd.donatracker.models.donation;
+
+
+import com.donatracker.a3even2odd.donatracker.models.location.Locations;
+
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+import static com.donatracker.a3even2odd.donatracker.models.donation.Donation.load;
+import static org.junit.Assert.*;
+
+public class DonationTest {
+
+
+
+    @Test
+    public void loadTest() {
+
+       LinkedList<Donation> donationNull = null;
+       load(donationNull);
+       assertTrue(Donation.getDonations().isEmpty());
+
+
+       LinkedList<Donation> donationfull = new LinkedList<>();
+       donationfull.add(new Donation());
+
+       load(donationfull);
+       //assertFalse(Donation.getDonations().isEmpty());
+       assertEquals(Donation.getDonations().size(), 1);
+    }
+}
+
+
+//        assertEquals – makes sure the two things compared are “equal”
+//        assertFalse – makes sure the thing has a value of “false”
+//        assertNotNull – makes sure the thing has a value
+//        assertNotSame – makes sure two things are not the actual same thing
+//        assertNull – makes sure the thing does not have a value
+//        assertTrue – makes sure the thing has a value of “true”
